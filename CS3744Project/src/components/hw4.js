67e8c5224428934b9549e4e296a0a6d51e9b7ba0
Nanimono99 <1667959646@qq.com>
2022-07-01 14:28:51 +0800

    Update hw4.js

--- CS3744Project/src/components/hw4.js
+++ CS3744Project/src/components/hw4.js
@@ -22,19 +22,21 @@ class YahooFinance extends Component {
         super(props);
         this.background = 'lightblack';
         this.myRef = React.createRef();
-        this.margin = {top: 10, right: 10, bottom: 40, left: 70}
-        this.pxX = 600 - this.margin.left - this.margin.right
-        this.pxY = 300 - this.margin.top - this.margin.bottom
+        this.margin = {top:40, right: 40, bottom: 40, left: 70}
+        this.pxX = 660 - this.margin.left - this.margin.right
+        this.pxY = 330 - this.margin.top - this.margin.bottom
         this.container = undefined;
+        // get values
         this.date = []
     }
 
-    init = () => {
+    initiate = () => {
         let root = this.myRef.current;
         this.container = d3.select(root).append('svg')
-            .attr('width', 600 + this.margin.left + this.margin.right).attr('height', +
-            300 + this.margin.top + this.margin.bottom)
-            .style('background', this.background)
+            .attr('width', 600 + this.margin.left + this.margin.right)
+            .attr('height', +300 + this.margin.top + this.margin.bottom)
+            .style('background', 
+            this.background)
             .append('g')
             .attr('transform', `translate(${this.margin.left} ${this.margin.top + 30})`)
 
@@ -46,9 +48,14 @@ class YahooFinance extends Component {
         }
         )
         this.date.unshift(new Date(this.date[0].getTime() - (24 * 60 * 700 * 1000)))
-        this.date.push(new Date(this.date[this.date.length - 1].getTime() + (24 * 60 * 100 * 1000)))
+        this.date.push(new Date(this.date[this.date.length - 1]
+                 .getTime() + (24 * 60 * 100 * 1000)))
 
-        let scX = d3.scaleTime().domain([this.date[0], this.date[this.date.length - 1]]).range([0, this.pxX])
+
+        // Set scaling for time
+        let scX = d3.scaleTime()
+                    .domain([this.date[0], this.date[this.date.length - 1]])
+                    .range([0, this.pxX])
 
         let [minVal, maxVal] = [Infinity, -Infinity]
         comparisons.forEach(tick => {
@@ -58,13 +65,17 @@ class YahooFinance extends Component {
                 }
             )
         })
-
-        let scY = d3.scaleLinear().domain([0, maxVal + (3000 - maxVal)]).range([this.pxY, 0])
+        let scY = d3.scaleLinear()
+                    .domain([0, maxVal + (3000 - maxVal)])
+                    .range([this.pxY, 0])
         let color = ['red', 'blue', 'yellow', 'green']
+        
 
+        // generating circles for each value in high
         comparisons.forEach((tick, idx) => {
             let { high, symbol } = tick
-            let g = this.container.append("g")
+            let g = this.container
+                .append("g")
             g.selectAll('circle')
                 .data(high)
                 .enter()
@@ -77,6 +88,7 @@ class YahooFinance extends Component {
                     .attr('opacity', 1)
             }
 
+            // generating lines for each value in high
             for (let i = 2; i < high.length; i++) {
                 g.append('line')
                     .attr('x1', scX(this.date[i]))
@@ -87,13 +99,14 @@ class YahooFinance extends Component {
                     .attr('stroke-width', 1.5)
                     .attr('opacity', 1)
             }
-
+            // our tooltip
             let toolTip = this.container.append('text')
             g.selectAll('circle')
                 .on('mouseover', function (e) {
                     let [x, y] = d3.pointer(e, this)
 
-                    let yVal = scY.invert(y).toFixed(2);
+                    let yVal = scY.invert(y)
+                                  .toFixed(2);
                     toolTip.attr('x', x)
                         .attr('y', y)
                         .text(yVal)
@@ -108,11 +121,11 @@ class YahooFinance extends Component {
                 })
             g.append('text')
                 .attr('x', this.pxX + 20)
-                .attr('y', 200 - idx * 20)
+                .attr('y', 200 - idx * 30)
                 .attr("fill", color[idx])
                 .text(symbol)
         })
-
+        // adding ticks and axis
         let axis = d3.axisRight(scY)
         this.container.append('g').call(axis)
         axis = d3.axisTop(scX)
@@ -122,19 +135,26 @@ class YahooFinance extends Component {
             .selectAll('text')
             .attr('transform', 'rotate(90)translate(20, 0)')
 
-        const xAxisGrid = d3.axisBottom(scX).tickSize(-this.pxX + 270).tickFormat('').ticks(10);
+        const xAxisGrid = d3.axisBottom(scX)
+                            .tickSize(-this.pxX + 300)
+                            .tickFormat('')
+                            .ticks(10);
         this.container.append('g')
-            .attr('class', 'x, axis-grid')
-            .attr('transform', `translate(0, ${this.pxY})`).call(xAxisGrid)
+                      .attr('class', 'x, axis-grid')
+                      .attr('transform', `translate(0, ${this.pxY})`)
+                      .call(xAxisGrid)
 
-        const yAxisGrid = d3.axisLeft(scY).tickSize(-this.pxY - 270).tickFormat('').ticks(10);
+        const yAxisGrid = d3.axisLeft(scY)
+                            .tickSize(-this.pxY - 300)
+                            .tickFormat('')
+                            .ticks(10);
         this.container.append('g')
             .attr('class', 'y, axis-grid')
             .call(yAxisGrid)
     }
 
     componentDidMount() {
-        this.init()
+        this.initiate()
     }
 
     render() {
