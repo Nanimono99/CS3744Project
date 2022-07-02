f6d98a8edaa0ceddfdd57d2f762c5d64ea1cc1b4
Nanimono99 <1667959646@qq.com>
2022-07-03 00:42:35 +0800

    Update index.js

--- CS3744Project/src/index.js
+++ CS3744Project/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { GridFunction } from './components/hw2';
-import { Bug } from './components/hw3';
+
 import MyMain from "./components/Main";
 import Page from './components/Page';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import NasaClass from './components/hw1';
 import  Project from './components/project';
-import YahooFinance from './components/hw4';
-const data = require('./json/finance.json');
+// import YahooFinance from './components/hw4';
+// import { GridFunction } from './components/hw2';
+// import { Bug } from './components/hw3';
+// const data = require('./json/finance.json');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,9 +19,9 @@ root.render(
                 <Route index element={<MyMain />}/>
                 <Route path={"/project"} element={<Project />}/>
                 <Route path={"/project/1"} element={<NasaClass />}/>
-                <Route path={"/project/2"} element={<GridFunction />}/>
+                {/* <Route path={"/project/2"} element={<GridFunction />}/>
                 <Route path={"/project/3"} element={<Bug />}/>
-                <Route path={"/project/4"} element={<YahooFinance data={data}/>}/>
+                <Route path={"/project/4"} element={<YahooFinance data={data}/>}/> */}
             </Route>
         </Routes>
     </BrowserRouter>
